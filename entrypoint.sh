5dad5888f0c914c291df7d0419d700db4fcbe776
Zero-Trust <62760177+Zero-Trust@users.noreply.github.com>
2020-08-10 10:53:32 +0900

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+# (Execute inside a Docker Container)
+
+UPLOAD_FILE="$1"
+GITHUB_USERNAME="$2"
+GITHUB_REPO="$3"
+USER_EMAIL="$4"
+
+# Setup git repository
+git config --global user.name "${GITHUB_USERNAME}"
+git config --global user.email "${USER_EMAIL}"
+
+# Clone 
+git clone "https://${API_TOKEN_GITHUB}@github.com/${GITHUB_USERNAME}/${GITHUB_REPO}.git" ${UPLOAD_FILE}
+
+
+git add ${UPLOAD_FILE}
+git commit --message "Upload from https://github.com/${GITHUB_REPOSITORY}/commit/${GITHUB_SHA} by GitHub Actions"
+git push origin master
+
+
